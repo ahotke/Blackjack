c74e85f5987b3d74d25b0ae8e8a22198d7cb7f45
Ashton Hotke <339741886@GoTVDSB.ca>
2025-01-09 09:32:29 -0500

    - Got card drawing working - Worked on intro screen

--- Blackjack Royale/Game1.cs
+++ Blackjack Royale/Game1.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
-namespace Blackjack
+namespace Blackjack_Royale
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Rectangle sourceRectangle;
+        Texture2D cardSpritesheet;
+        Texture2D cropTexture;
+        Texture2D logoTexture;
+        Texture2D playTexture;
+        Rectangle sourceRect, logoRect, playRect;
+
+        enum Screen {intro, casino, end};
+
+        Screen screen;
+
+        List<Texture2D> cardTextures;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -20,12 +31,36 @@ namespace Blackjack
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            
             base.Initialize();
+
+            screen = Screen.casino;
+            cardTextures = new List<Texture2D>();
+
+            logoRect = new Rectangle(240, 0, 325, 325);
+            playRect = new Rectangle(272, 325, 250, 100);
+
+            int width = cardSpritesheet.Width / 13;
+            int height = cardSpritesheet.Height / 5;
+            for (int y = 0; y < 5; y++)
+                for (int x = 0; x < 13; x++)
+                {
+                    sourceRect = new Rectangle(x * width, y * height, width, height);
+                    Texture2D cropTexture = new Texture2D(GraphicsDevice, width, height);
+                    Color[] data = new Color[width * height];
+                    cardSpritesheet.GetData(0, sourceRect, data, 0, data.Length);
+                    cropTexture.SetData(data);
+                    if (cardTextures.Count < 55)
+                        cardTextures.Add(cropTexture);
+                }
         }
 
         protected override void LoadContent()
         {
+            cardSpritesheet = Content.Load<Texture2D>("card_deck");
+            logoTexture = Content.Load<Texture2D>("blackjack_symbol");
+            playTexture = Content.Load<Texture2D>("play_button");
+           
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
@@ -36,17 +71,32 @@ namespace Blackjack
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            
+
+
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.DarkRed);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            if (screen == Screen.intro) 
+            {
+                _spriteBatch.Draw(logoTexture, logoRect, Color.White);
+                _spriteBatch.Draw(playTexture, playRect, Color.White);
+            }
+
+            if (screen == Screen.casino)
+            {
+                for (int i = 0; i < cardTextures.Count; i++)
+                    _spriteBatch.Draw(cardTextures[i], new Rectangle((0 + 60 * i), 0, 60, 100), Color.White);
+            }
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
